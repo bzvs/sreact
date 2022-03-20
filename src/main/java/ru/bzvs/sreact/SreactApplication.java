26a013771c05a0d14046037065982ea6ca746615
Bezzubenko Viacheslav <bezzubenko_slava@mail.ru>
2022-03-20 22:58:11 +0300

    initial commit

---
+++ src/main/java/ru/bzvs/sreact/SreactApplication.java
@@ -0,0 +1,13 @@
+package ru.bzvs.sreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bzvs/sreact/SreactApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bzvs.sreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
